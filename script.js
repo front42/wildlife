499be0b4a7963fbd703efc51353bb826421896d8
front42 <altaifront@gmail.com>
2024-07-14 00:42:32 +0300

    feat: hide buttons highlight

--- script.js
+++ script.js
@@ -2,36 +2,36 @@ const slides = document.querySelector('.slides');
 const arrowLeft = document.querySelector('.arrow-left');
 const arrowRight = document.querySelector('.arrow-right');
 
-const showRight = () => {
+const showLeft = () => {
   arrowLeft.disabled = true;
+  slides.lastElementChild.classList.add('thinner');
+  slides.prepend(slides.lastElementChild);
+  setTimeout(() => slides.firstElementChild.classList.remove('thinner'));
+  setTimeout(() => arrowLeft.disabled = false, 700);
+};
+
+const showRight = () => {
+  arrowRight.disabled = true;
   slides.firstElementChild.classList.add('thinner');
   setTimeout(() => {
     slides.append(slides.firstElementChild);
     slides.lastElementChild.classList.remove('thinner');
-    arrowLeft.disabled = false;
+    arrowRight.disabled = false;
   }, 700);
-};
-
-const showLeft = () => {
-  arrowRight.disabled = true;
-  slides.lastElementChild.classList.add('thinner');
-  slides.prepend(slides.lastElementChild);
-  setTimeout(() => slides.firstElementChild.classList.remove('thinner'));
-  setTimeout(() => arrowRight.disabled = false, 700);
 };
 
 arrowLeft.addEventListener('click', showLeft);
 arrowRight.addEventListener('click', showRight);
 
-const menu = document.querySelector('.menu');
 const burger = document.querySelector('.burger');
-
-menu.addEventListener('click', () => {
-  menu.classList.remove('active');
-  burger.classList.remove('active');
-});
+const menu = document.querySelector('.menu');
 
 burger.addEventListener('click', () => {
   menu.classList.toggle('active');
   burger.classList.toggle('active');
+});
+
+menu.addEventListener('click', () => {
+  menu.classList.remove('active');
+  burger.classList.remove('active');
 });
